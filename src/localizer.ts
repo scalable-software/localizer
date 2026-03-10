927509ecd54035b3f2d4e8919b0acf323e2cb1a0
Architect <solution.architect@scalable.software>
2026-03-10 17:19:57 +0100

    STATE: localizer.language return by default the language of the browser -> PASS

--- src/localizer.ts
+++ src/localizer.ts
@@ -3,7 +3,16 @@
  * @category Utilities
  */
 export class Localizer<T extends object> extends EventTarget {
-  public get language(): string {
-    return "";
+  private _language: string;
+
+  constructor() {
+    super();
+
+    this._language = this._normalize(navigator.language);
   }
+  public get language(): string {
+    return this._language;
+  }
+
+  private _normalize = (locale: string) => locale.split("-")[0].toLowerCase();
 }

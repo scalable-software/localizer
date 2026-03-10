f7c0e5f1fff821c4a9a67862d13da78544aae09b
Architect <solution.architect@scalable.software>
2026-03-10 17:24:42 +0100

    STATE: localizer.language setter exist -> PASS

--- src/localizer.ts
+++ src/localizer.ts
@@ -10,8 +10,12 @@ export class Localizer<T extends object> extends EventTarget {
 
     this._language = this._normalize(navigator.language);
   }
+
   public get language(): string {
     return this._language;
+  }
+  public set language(language: string) {
+    if (!language) return;
   }
 
   private _normalize = (locale: string) => locale.split("-")[0].toLowerCase();

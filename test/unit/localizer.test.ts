a1210ba4d179c248bddbaaed14808f0791fea093
Architect <solution.architect@scalable.software>
2026-03-10 17:06:30 +0100

    STATE: localizer.langauge exist -> FAIL

--- test/unit/localizer.test.ts
+++ test/unit/localizer.test.ts
@@ -1,1 +1,14 @@
-import { Localizer } from "@scalable.software/localizer";
+import { Localizer, State } from "@scalable.software/localizer";
+
+state(State.LANGUAGE, () => {
+  given("Localizer instantiated", () => {
+    let localizer: Localizer<object>;
+    beforeEach(() => {
+      localizer = new Localizer();
+    });
+
+    then("localizer.language is defined", () => {
+      expect(localizer.language).toBeDefined();
+    });
+  });
+});

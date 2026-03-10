ec26a2ec4c6f32fba3c31c62b68259ca57fc1a4e
Architect <solution.architect@scalable.software>
2026-03-10 17:06:35 +0100

    STATE: localizer.langauge exist -> PASS

--- src/localizer.ts
+++ src/localizer.ts
@@ -2,4 +2,8 @@
  * Localizer addon for components
  * @category Utilities
  */
-export class Localizer<T extends object> extends EventTarget {}
+export class Localizer<T extends object> extends EventTarget {
+  public get language(): string {
+    return "";
+  }
+}
